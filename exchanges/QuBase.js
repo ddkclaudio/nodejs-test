d2be13268fefbc34589bf25dc7a2ba6dd1dd1693
Claudio Silva <ddk.claudio@gmail.com>
2019-02-07 11:37:29 -0200

    Timer to save

--- exchanges/QuBase.js
+++ exchanges/QuBase.js
@@ -1,17 +1,30 @@
 module.exports = class QuBase {
     constructor(timeToSaveInSeconds = 1) {
-        this.timeToSaveInSeconds = timeToSaveInSeconds * 1000
+        this.timeToSaveInSeconds = timeToSaveInSeconds
+        this.timeoutToSave = null
+    }
+
+    setTimeoutToSave(timeToSaveInSeconds, self = this) {
+        if (this.timeoutToSave != null)
+            clearInterval(this.timeoutToSave);
+
+        this.timeoutToSave = setInterval(() => {
+            self._save()
+        }, timeToSaveInSeconds * 1000);
     }
 
     onTrades(trade = { symbol: null, trades: null }) {
+        if (this.timeoutToSave == null)
+            this.setTimeoutToSave(this.timeToSaveInSeconds)
+
         console.log("QuBase.onTrades()", "t" + trade.symbol, `trades: ${JSON.stringify(trade.trades)}`)
     }
 
     onOrderBook(orderBook = { symbol: null, orderBook: null }) {
-        console.log("QuBase.onOrderBook()", "t" + orderBook.symbol, `trades: ${JSON.stringify(orderBook.orderBook)}`)
+        // console.log("QuBase.onOrderBook()", "t" + orderBook.symbol, `trades: ${JSON.stringify(orderBook.orderBook)}`)
     }
 
-    save() {
-        console.log("QuBase", "save()");
+    _save() {
+        console.log("QuBase", "save()", "=============================");
     }
 }
