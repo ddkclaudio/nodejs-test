545ab4cad0741bd7dd3b3bdbb15f9308d013f189
Claudio Silva <ddk.claudio@gmail.com>
2019-02-10 19:11:27 -0200

    Pegando Trades e normalizando
    Testado para todas as moedas simultaneamente

--- exchanges/binance/main.js
+++ exchanges/binance/main.js
@@ -1,23 +1,60 @@
-var logger = require('logger').createLogger('/home/qu/logs/qu_binance.log'); // logs to a file
+const Binance = require('node-binance-api');
+const binance = new Binance();
+global.ticker = {};
+var markets = []
 
-var express = require("express");
-var app = express();
-var path = require("path");
-
-if(process.env.NODE_PORT)
-	var port = process.env.NODE_PORT
-else
-	var port = 3000
-
-app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname + '/index.html'));
+binance.prices((error, ticker) => {
+    if (error)
+        return
+    // PEGANDO TODOS OS SYMBOLOS
+    const symbols = Object.keys(ticker)
+    markets = symbols
+    // markets.push("BTCUSDT")
+    // markets.push("ETHBTC")
+    joinTT()
 });
 
-app.listen(port);
+function joinTT() {
+    // Subscribe to trades endpoint for all markets
+    binance.websockets.trades(markets, (trades) => {
+        let {
+            "e": EventType,
+            "p": Price,
+            "q": Quantity,
+            "T": Tradetime,
+            "m": IsTheBuyerTheMarketMaker,
+            "t": TradeID,
+            "b": BuyerorderID,
+            "a": SellerorderID,
+            "E": EventTime,
+            "s": Symbol,
+            "M": Ignore
+        } = trades;
 
-console.log("Running at Port " + port);
+        if (EventType != "trade")
+            return
 
-setInterval(function () {
-    logger.info('ping', 'PORT_NUMBER',port);
-}, 3000);
+        const normalized = {
+            // BASIC INFOS
+            order_id: TradeID,
+            timestamp: Tradetime,
+            amount: (IsTheBuyerTheMarketMaker) ? Quantity : -Quantity,
+            price: Price,
+            // EXTRAS
+            buyer_order_id: BuyerorderID,
+            seller_order_id: SellerorderID,
+            event_time: EventTime,
+            symbol: Symbol,
+            ignore: Ignore,
+            is_the_buyer_the_market_maker: IsTheBuyerTheMarketMaker
+        }
+
+        console.log(Symbol)
+    });
+
+    // You can use global.ticker anywhere in your program now
+    setInterval(() => {
+        // console.log("*** Price of BTC: " + global.ticker.BTCUSDT);
+    }, 3000);
+}
 
